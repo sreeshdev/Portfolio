b9793b3380cc107ac8e5ef43ddbba7cd29f70d43
sreenaath <writetosreesh@gmail.com>
2019-03-20 17:41:27 +0530

    Updates

---
+++ precache-manifest.a061e29f7062989e337b2029a20f3ae5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc0176f25ea33aeb5338",
+    "url": "/portfolio/static/css/main.176444eb.chunk.css"
+  },
+  {
+    "revision": "bc0176f25ea33aeb5338",
+    "url": "/portfolio/static/js/main.3ab2cc9d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "78700644d2c1f9c21ef1",
+    "url": "/portfolio/static/js/2.ba6cc00b.chunk.js"
+  },
+  {
+    "revision": "53219bab9cdd0812294d354cb07f9ab8",
+    "url": "/portfolio/static/media/icon.53219bab.png"
+  },
+  {
+    "revision": "607b4d392a6a7018b5778a5e57c22db9",
+    "url": "/portfolio/static/media/sree logo.607b4d39.svg"
+  },
+  {
+    "revision": "1090920ba35b38565356638528f2792b",
+    "url": "/portfolio/static/media/Me.1090920b.jpg"
+  },
+  {
+    "revision": "a787d9152cca35ce0324b17807ee644c",
+    "url": "/portfolio/static/media/ket1.a787d915.png"
+  },
+  {
+    "revision": "bec227d3bfebdc38aa4cc85cea3b8eae",
+    "url": "/portfolio/static/media/skillbg.bec227d3.jpg"
+  },
+  {
+    "revision": "ad965d6f42a5ae7c4c13d09942c6d43d",
+    "url": "/portfolio/static/media/thetraining_logo.ad965d6f.svg"
+  },
+  {
+    "revision": "3f180720e839637c389b940277b86e92",
+    "url": "/portfolio/index.html"
+  }
+];
