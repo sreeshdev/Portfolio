8154f3259a4921d88f5eb26557c6ef474c8bb7d4
sreenaath <writetosreesh@gmail.com>
2019-06-16 08:52:47 +0530

    Updates

---
+++ precache-manifest.843ff57fb8d90d18aa192d691c415b5a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab776c66d1360325165a214b5ba483a2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "44c9a64a276536a7184d",
+    "url": "/portfolio/static/css/main.6885ce1e.chunk.css"
+  },
+  {
+    "revision": "0ef923c0ccf77ed1e910",
+    "url": "/portfolio/static/js/2.a2cb28a7.chunk.js"
+  },
+  {
+    "revision": "44c9a64a276536a7184d",
+    "url": "/portfolio/static/js/main.36b568bb.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "25816d506143e0f074057deff56a1450",
+    "url": "/portfolio/static/media/Me.25816d50.jpg"
+  },
+  {
+    "revision": "53219bab9cdd0812294d354cb07f9ab8",
+    "url": "/portfolio/static/media/icon.53219bab.png"
+  },
+  {
+    "revision": "a787d9152cca35ce0324b17807ee644c",
+    "url": "/portfolio/static/media/ket1.a787d915.png"
+  },
+  {
+    "revision": "bec227d3bfebdc38aa4cc85cea3b8eae",
+    "url": "/portfolio/static/media/skillbg.bec227d3.jpg"
+  },
+  {
+    "revision": "6c4158ae2b41d1e9f0d35503af989b09",
+    "url": "/portfolio/static/media/slogo.6c4158ae.png"
+  },
+  {
+    "revision": "ad965d6f42a5ae7c4c13d09942c6d43d",
+    "url": "/portfolio/static/media/thetraining_logo.ad965d6f.svg"
+  }
+]);
